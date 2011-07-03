e671d79f1c1e047a465b3a118843f7dde4967b52
keisatou <hoge@server>
2011-07-04 00:33:22 +0900

    have read p.90

--- htdocs/php/index.php
+++ htdocs/php/index.php
@@ -1,2 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" http://www.w3.org/TR/xhtml1/xhtml1-Transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>hello, world!</title>
+</head>
+
+<body>
 <?php
 echo "hello, world";
+?>
+</body>
+
+</html>
